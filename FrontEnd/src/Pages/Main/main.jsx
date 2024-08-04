d95caa2d2d46e90c383cf12677b851bd5aaddfe8
juanlezama1 <juanpablolezama92@gmail.com>
2024-08-04 15:00:04 -0300

    Sigo con la página inicial

--- FrontEnd/src/Pages/Main/main.jsx
+++ FrontEnd/src/Pages/Main/main.jsx
@@ -1,9 +1,19 @@
 import Products_carousel from "../../Components/Products_carousel/products_carousel"
+import Main_Titles from '../../Components/Main_Titles/main_titles'
+import Container from "react-bootstrap/esm/Container"
 
 const Main = () => {
 
     return (
-        <Products_carousel/>
+        <Container fluid>
+            <div className="row">
+                <div className="col-12 m-0 p-0 overflow-hidden">
+                    <Main_Titles title={"Proyecto Final CoderHouse - Juguetería D'Kids"}></Main_Titles>
+                    <Products_carousel/>
+                </div>
+            </div>
+
+        </Container>
     )
 }
 
